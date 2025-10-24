3f940e692807466f2fcd5d353ae77b33452e6267
dev.takaya.inoue <dev.takaya.inoue@gmail.com>
2025-10-25 07:20:08 +0900

    Remove archived directory

--- archived/common/driver.py
+++
@@ -1,7 +0,0 @@
-"""
-互換性維持のためのラッパ。新しいコードは ``common.drivers`` を参照。
-"""
-
-from common.drivers import CncDriver
-
-__all__ = ["CncDriver"]
--- archived/common/utils.py
+++
@@ -1,5 +0,0 @@
-"""
-ユーティリティ（設定読み込み・ファイル選択・ログ設定等）
-"""
-
-# ここにselect_config_interactive, setup_logging等を実装
